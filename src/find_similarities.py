fe4145065699e7b375c23bf096393b5c271a7038
elisar.chodorov <elisar.chodorov@riskified.com>
2023-01-27 08:06:20 +0200

    new matcher

--- src/find_similarities.py
+++ src/find_similarities.py
@@ -24,58 +24,25 @@ class ObsMatcher:
         intervention_group: Series = p_scores[label]
         control_group: Series = p_scores[~label]
 
-        result, match_ids = [], []
+        match_idss = {}
         print('starting matching process, this might take a time')
-        for index, score in intervention_group.items():
+        popi = intervention_group.head(4)  # .items()
+        for index, score in tqdm(popi.items()):  # intervention_group.items()
             matches: Series = abs(control_group - score).sort_values()
+
             in_caliper: Series = matches <= self.caliper
             matches_in_caliper: Series = matches[in_caliper]
             matches_in_caliper_n = matches_in_caliper.head(self.n_matches)
             if len(matches_in_caliper_n) == 0:
-                continue
-
-            if len(matches_in_caliper_n) < self.n_matches:
-                select: int = len(matches_in_caliper_n)
-            else:
-                select: int = self.n_matches
-
-            chosen = random.choice(matches_in_caliper_n.index, min(select, self.n_matches), replace=False)
-
-            result.extend([intervention_group.index[i]] + list(chosen))
-            match_ids.extend([i] * (len(chosen) + 1))
+                break
+            select: int = min(len(matches_in_caliper_n), self.n_matches)
+            chosen = random.choice(matches_in_caliper.index, select, replace=False)
+            match_idss.append({index: list(chosen)})
             control_group = control_group.drop(index=chosen)
 
-
-
-
-
-
-        for i in tqdm(range(len(intervention_group))):
-            score = intervention_group.iloc[i]
-            # todo: add potential matches according to caliper
-            matches: Series = abs(control_group - score).sort_values()
-            in_caliper: Series = matches <= self.caliper
-            matches_in_caliper: Series = matches[in_caliper]
-            matches_in_caliper_n = matches_in_caliper.head(self.n_matches)
-
-            if len(matches_in_caliper_n) == 0:
-                continue
-
-            if len(matches_in_caliper_n) < self.n_matches:
-                select: int = len(matches_in_caliper_n)
-            else:
-                select: int = self.n_matches
-
-            chosen = random.choice(matches_in_caliper_n.index, min(select, self.n_matches), replace=False)
-
-            result.extend([intervention_group.index[i]] + list(chosen))
-            match_ids.extend([i] * (len(chosen) + 1))
-            control_group.drop(index=chosen, inplace=True)
-
-        matched_data = p_scores.loc[result]
-        matched_data['match_id'] = match_ids
-        matched_data['record_id'] = matched_data.index
-
-        print(f'please note:'
-              f'the matched dataset contains {matched_data.index.isin(label).sum()} observations from minority class')
-        return matched_data
+        matched_ids = DataFrame(match_idss)
+        print(
+            f'please note:'
+            f'the matched dataset contains {matched_ids.index.isin(label).sum()} observations from minority class'
+            )
+        return matched_ids
